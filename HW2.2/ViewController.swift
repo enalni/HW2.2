0279e27b4085a6eda5b486a7f979475bda233f4f
aa <aaa>
2020-03-01 16:47:14 +0300

    Initial Commit

---
+++ HW2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.2
+//
+//  Created by Александр Николаевич on 01.03.2020.
+//  Copyright © 2020 Alexander Nikolaevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
